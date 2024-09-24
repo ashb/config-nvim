b4b485b0ff75423f8ad5317ede5b0b95678f8103
Ash Berlin-Taylor <ash_github@firemirror.com>
2024-09-24 11:27:34 +0100

    Remove LSP diagnostic line plugins
    They never quite worked right, and the jumping as I navigated around wast too
    distracting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,25 +9,6 @@ local specs = {
     end,
     keys = require("mappings").lsp,
   },
-  -- Show more than one diagnostic on current line
-  {
-    -- This on needs to be loaded before nvim-lspconfig else it doesn't seem to work :shrug:
-    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-    event = { "LspAttach" },
-    config = function()
-      require("lsp_lines").setup()
-      vim.diagnostic.config { virtual_lines = { only_current_line = true } }
-    end,
-  },
-  -- And since we have the above, hide the virtual text one the current line
-  {
-    "luozhiya/lsp-virtual-improved.nvim",
-    event = { "LspAttach" },
-    config = function()
-      require("lsp-virtual-improved").setup()
-      vim.diagnostic.config { virtual_text = false, virtual_improved = { prefix = "", current_line = "hide" } }
-    end,
-  },
   -- "Peek" at impl/definition etc instead of go straight ther
   {
     "dnlhc/glance.nvim",
