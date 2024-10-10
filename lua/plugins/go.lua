e0087147fa02e284095dfd3b86ceaf4200ab06b2
Ash Berlin-Taylor <ash_github@firemirror.com>
2024-10-10 11:36:52 +0100

    Install ray-x/go plugin for go and gomod

---
+++ lua/plugins/go.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      tag_options = 'json=', -- It has `omitempty` here by default, I don't want that
+    },
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+}
