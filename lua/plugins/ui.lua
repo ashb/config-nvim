d499b9d58be346e5cc2f481f513b7309d4cb9e77
Ash Berlin-Taylor <ash_github@firemirror.com>
2025-10-06 16:44:28 +0100

    Remvoe folke/flash.nvim
    I wasn't using it, and ended up hitting it's hot keys without meaning to one
    too many times

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -177,28 +177,6 @@ local M = {
     },
   },
 
-  -- Visual jumping to selctions and ranges
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    ---@type Flash.Config
-    opts = {
-      label = {
-        rainbow = {
-          enabled = true,
-        },
-      },
-    },
-    -- stylua: ignore
-    keys = {
-      { "gs", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "gS", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-    },
-  },
-
   -- Pretty display of Markdown files inline in the buffer
   {
     "MeanderingProgrammer/render-markdown.nvim",
